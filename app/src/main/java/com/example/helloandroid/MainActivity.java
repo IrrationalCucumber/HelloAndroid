6b005907d2b79647685f29972b7fa6d7ce889473
Adrean <adreanpaulsorono@gmail.com>
2023-02-11 13:18:53 +0800

    Adrean Paul

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "Activity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v(TAG, "verbose nga log");
+        Log.d(TAG, "debug nga log" );
+        Log.i(TAG, "info nga log");
+        Log.e(TAG, "error nga logg");
+        Log.w(TAG, "warn nga log");
+
+        Button button = (Button) findViewById(R.id.button3);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "button clicker");
+            }
+        });
     }
 }
