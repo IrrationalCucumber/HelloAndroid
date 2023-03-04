6746d43a349471e737e14dae346c039589c9317d
Admin <adreanpaulsorono@gmai.com>
2023-03-04 11:49:44 +0800

    Adrean Paul

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i(TAG, "button clicker");
+                Log.i(TAG, "hala..ni click ang user...");
 
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
                 startActivity(intent);
