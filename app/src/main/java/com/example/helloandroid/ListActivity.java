a2556f10707404e28f57a6d0ccf4f0dcc2ffe8ac
Adrean <adreanpaulsorono@gmail.com>
2023-03-11 13:51:55 +0800

    Adrean Paul 03-04-23

--- app/src/main/java/com/example/helloandroid/ListActivity.java
+++ app/src/main/java/com/example/helloandroid/ListActivity.java
@@ -1,14 +1,35 @@
 package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import android.os.Bundle;
 
 public class ListActivity extends AppCompatActivity {
 
+    ListView l;
+    String tutorials[]
+            = { "Algorithms", "Data Structures",
+            "Languages", "Interview Corner",
+            "GATE", "ISRO CS",
+            "UGC NET CS", "CS Subjects",
+            "Web Technologies" };
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        l = findViewById(R.id.list);
+        ArrayAdapter<String> arr;
+        arr
+                = new ArrayAdapter<String>(
+                this,
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                tutorials);
+        l.setAdapter(arr);
     }
 }
---
+++ app/src/main/java/com/example/helloandroid/MainActivityFragment.java
@@ -0,0 +1,48 @@
+package com.example.helloandroid;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+
+public class MainActivityFragment extends AppCompatActivity {
+
+    Button firstFragment, secondFragment;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firstFragment = (Button) findViewById(R.id.FirstFragment);
+        secondFragment = (Button) findViewById(R.id.SecondFragment);
+
+        firstFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FirstFragment());
+            }
+        });
+        secondFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new SecondFragment());
+            }
+        });
+        }
+        private void loadFragment( Fragment fragment ){
+            FragmentManager fm = new getFragmentManager();
+            FragmentTransaction fragmentTransaction = fm.beginTransaction();
+            fragmentTransaction.replace(R.id.frameLayout, fragment);
+            fragmentTransaction.commit();
+
+
+    }
+}
